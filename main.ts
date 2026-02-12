1cfb366ef7b2226d2ba8b18b910359c61029b936
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-02-12 13:02:16 -0500

    changed ability to kill zombie1

--- main.ts
+++ main.ts
@@ -1,25 +1,60 @@
 namespace SpriteKind {
     export const weapon = SpriteKind.create()
+    export const Zombie = SpriteKind.create()
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . 1 1 1 . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, Player1, 50, 50)
+    projectile.follow(Zombie1, 100)
+    if_projectile_hits_Zombie1 += 1
 })
 info.onScore(100, function () {
     info.setScore(0)
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.weapon, function (sprite, otherSprite) {
-    info.startCountdown(5)
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Zombie, function (sprite, otherSprite) {
     game.gameOver(true)
-    game.reset()
 })
+let if_projectile_hits_Zombie1 = 0
+let projectile: Sprite = null
+let Zombie1: Sprite = null
+let Player1: Sprite = null
 scene.setBackgroundImage(assets.image`background`)
-let mySprite = sprites.create(assets.image`Player 1`, SpriteKind.Player)
-controller.moveSprite(mySprite, 100, 100)
-let mySprite2 = sprites.create(assets.image`Gun`, SpriteKind.weapon)
-mySprite2.setPosition(13, 17)
-mySprite2.setPosition(72, 50)
-mySprite2.setPosition(120, 45)
-mySprite2.setPosition(10, 98)
-info.startCountdown(5)
-music.play(music.createSong(hex`0078000408020200001c00010a006400f4016400000400000000000000000000000000050000040c0004000800012008000c00012407001c00020a006400f401640000040000000000000000000000000000000003190008000c00012a0c001000021e27100014000120140018000125`), music.PlaybackMode.UntilDone)
-game.showLongText("Domain Expansion", DialogLayout.Center)
+Player1 = sprites.create(assets.image`Player 1`, SpriteKind.Player)
+controller.moveSprite(Player1, 100, 100)
+info.startCountdown(90)
+Zombie1 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . 7 7 7 7 7 7 7 7 7 . . . . 
+    . . . 7 7 7 7 7 7 7 7 7 . . . . 
+    . . . 7 7 7 7 7 7 7 7 7 . . . . 
+    . . . 7 7 7 7 7 7 7 7 7 . . . . 
+    . . . 7 7 7 7 7 7 7 7 7 . . . . 
+    . . . 7 7 7 7 7 7 7 7 7 . . . . 
+    . . . 7 7 7 7 7 7 7 7 7 . . . . 
+    . . . 7 7 7 7 7 7 7 7 7 . . . . 
+    . . . 7 7 7 7 7 7 7 7 7 . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Zombie)
+Zombie1.setPosition(142, 8)
+Zombie1.follow(Player1, 75)
